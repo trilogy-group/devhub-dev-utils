af41d6ce762110f103306d052aead845ad74dc6f
Javier Matias <jmatias@thoughtworks.com>
2020-05-30 11:32:56 -0400

    More ignores

--- .config/JetBrains/IntelliJIdea2020.1/scratches/scratches/buffer1.ps1
+++
@@ -1,1 +0,0 @@
--l:1 -u:SU --quiet --serial aQOI-rvYv-zDYk-ZQOz-XUvw-zvZv-F1ZN-IOcu
--- .config/JetBrains/IntelliJIdea2020.1/scratches/scratches/guat.ps1
+++
@@ -1,3 +0,0 @@
-Get-VSSetupInstance | Select-VSSetupInstance -Latest | select -Property InstanceId,InstallationPath | Tee-Object -Variable myVar
-Import-Module -Name  "$($myVar.InstallationPath)\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell $myvar.InstanceId -StartInPath $PWD
