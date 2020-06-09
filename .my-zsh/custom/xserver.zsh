db77bbe854ea82d3adbe496918f344e292027176
Javier Matias <jmatias@thoughtworks.com>
2020-06-09 16:36:59 -0400

    Updated eksctl completion script

--- .my-zsh/custom/xserver.zsh
+++ .my-zsh/custom/xserver.zsh
@@ -1,4 +1,5 @@
 # Get the IP address of the host from /etc/resolv.conf
 export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
+export LIBGL_ALWAYS_INDIRECT=1
 # # Set the display path
 export DISPLAY=$WSL_HOST:0.0
