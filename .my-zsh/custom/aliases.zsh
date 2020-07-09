92d26d2e71c0ad971e54975f5255895a2d072c70
Javier Matias <jmatias@thoughtworks.com>
2020-07-08 21:54:23 -0400

    Add output color.

--- .my-zsh/custom/aliases.zsh
+++ .my-zsh/custom/aliases.zsh
@@ -14,3 +14,9 @@ alias awk4='awk '"'"'{print $4}'"'"''
 alias awk5='awk '"'"'{print $5}'"'"''
 
 alias weather='curl wttr.in'
+
+function gsquash() {
+  commits=$1
+  shift
+  git reset --soft HEAD~$commits && git commit --edit -m"$(git log --format=%B --reverse HEAD..HEAD@{1})"
+}
